a4b81cb0cd9c70f29df969a4b88cb78ecbd03374
vajo <vajosimic123@gmail.com>
2022-12-21 15:44:28 +0100

    Prvo dodavanje1

---
+++ domaci/Stred.c
@@ -0,0 +1,184 @@
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
+#include <linux/kdev_t.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
+#include <linux/device.h>
+#include <linux/wait.h>
+#include <linux/semaphore.h>
+#define BUFF_SIZE 100
+MODULE_LICENSE("Dual BSD/GPL");
+
+dev_t my_dev_id;
+static struct class *my_class;
+static struct device *my_device;
+static struct cdev *my_cdev;
+
+DECLARE_WAIT_QUEUE_HEAD(readQ);
+DECLARE_WAIT_QUEUE_HEAD(writeQ);
+struct semaphore sem;
+
+
+int stred_memory[BUFF_SIZE];
+int pos = 0;
+int endRead = 0;
+
+int stred_open(struct inode *pinode, struct file *pfile);
+int stred_close(struct inode *pinode, struct file *pfile);
+ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
+ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset);
+
+struct file_operations my_fops =
+{
+	.owner = THIS_MODULE,
+	.open = stred_open,
+	.read = stred_read,
+	.write = stred_write,
+	.release = stred_close,
+};
+
+
+int stred_open(struct inode *pinode, struct file *pfile) 
+{
+		printk(KERN_INFO "Succesfully opened stred\n");
+		return 0;
+}
+
+int stred_close(struct inode *pinode, struct file *pfile) 
+{
+		printk(KERN_INFO "Succesfully closed stred\n");
+		return 0;
+}
+
+ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
+{
+	int ret;
+	char buff[BUFF_SIZE];
+	long int len = 0;
+	if (endRead){
+		endRead = 0;
+		return 0;
+	}
+	
+	if(pos > 0)
+	{
+		pos --;
+		len = scnprintf(buff, BUFF_SIZE, "%d ", lifo[pos]);
+		ret = copy_to_user(buffer, buff, len);
+		if(ret)
+			return -EFAULT;
+		printk(KERN_INFO "Succesfully read\n");
+		endRead = 1;
+
+	return len;
+}
+
+ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
+{
+	char buff[BUFF_SIZE];
+	char podatak[BUFF_SIZE];
+	char funk[8];
+	int ret;
+
+	ret = copy_from_user(buff, buffer, length);
+	if(ret)
+		return -EFAULT;
+	buff[length-1] = '\0';
+
+	ret = sscanf(buff,"%10[^= ]=%99[^\t\n=]", funk, podatak);
+	printk(KERN_INFO "Funkcija je : %s\n", func);
+	printk(KERN_INFO "Podatak je : %s\n", podatak);
+	
+
+	if(ret==2)//two parameters parsed in sscanf
+	{
+		if(position >=0 && position <=9)
+		{
+			storage[position] = value; 
+			printk(KERN_INFO "Succesfully wrote value %d in position %d\n", value, position); 
+		}
+		else
+		{
+			printk(KERN_WARNING "Position should be between 0 and 9\n"); 
+		}
+	}
+	else
+	{
+		printk(KERN_WARNING "Wrong command format\nexpected: n,m\n\tn-position\n\tm-value\n");
+	}
+
+	return length;
+}
+
+static int __init stred_init(void)
+{
+   int ret = 0;
+	int i=0;
+	
+	sema_init(&sem,1);
+
+	//Initialize array
+	for (i=0; i<10; i++)
+		lifo[i] = 0;
+
+   ret = alloc_chrdev_region(&my_dev_id, 0, 1, "lifo");
+   if (ret){
+      printk(KERN_ERR "failed to register char device\n");
+      return ret;
+   }
+   printk(KERN_INFO "char device region allocated\n");
+
+   my_class = class_create(THIS_MODULE, "lifo_class");
+   if (my_class == NULL){
+      printk(KERN_ERR "failed to create class\n");
+      goto fail_0;
+   }
+   printk(KERN_INFO "class created\n");
+   
+   my_device = device_create(my_class, NULL, my_dev_id, NULL, "lifo");
+   if (my_device == NULL){
+      printk(KERN_ERR "failed to create device\n");
+      goto fail_1;
+   }
+   printk(KERN_INFO "device created\n");
+
+	my_cdev = cdev_alloc();	
+	my_cdev->ops = &my_fops;
+	my_cdev->owner = THIS_MODULE;
+	ret = cdev_add(my_cdev, my_dev_id, 1);
+	if (ret)
+	{
+      printk(KERN_ERR "failed to add cdev\n");
+		goto fail_2;
+	}
+   printk(KERN_INFO "cdev added\n");
+   printk(KERN_INFO "Hello world\n");
+
+   return 0;
+
+   fail_2:
+      device_destroy(my_class, my_dev_id);
+   fail_1:
+      class_destroy(my_class);
+   fail_0:
+      unregister_chrdev_region(my_dev_id, 1);
+   return -1;
+}
+
+static void __exit sred_exit(void)
+{
+   cdev_del(my_cdev);
+   device_destroy(my_class, my_dev_id);
+   class_destroy(my_class);
+   unregister_chrdev_region(my_dev_id,1);
+   printk(KERN_INFO "Goodbye, cruel world\n");
+}
+
+
+module_init(lifo_init);
+module_exit(lifo_exit);
